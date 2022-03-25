1e63b65796f675b341d421b94e451897ce040711
AidarS <nurotanec@bk.ru>
2022-03-26 00:54:44 +0600

    initial

---
+++ lesson01/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// Task
+const title = "iPhone";
+const amount = 5;
+const category = "Smartphones";
+const price = 1500;
+
+console.log('name: ', title);
+
+// Bonus
+const total = price * amount;
+console.log('Общая сумма товара: ', total);
