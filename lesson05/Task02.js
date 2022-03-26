9e02647020913b2b09f7ef2fa1424da1323be0f5
AidarS <nurotanec@bk.ru>
2022-03-26 18:12:03 +0600

    2nd task

--- lesson05/Task02.js
+++ lesson05/Task02.js
@@ -1,35 +1,11 @@
 'use strict';
 
-{
-    const rain = Math.round(Math.random());
-    if (rain === 1) {
-        console.log('Пошёл дождь. Возьмите зонт!');
+const capitalize = (sentence) => {
+    if (sentence !== '') {
+        const lower = String(sentence).toLowerCase();
+        return lower.replace(lower[0], lower[0].toUpperCase());
     }
-    if (rain === 0) {
-        console.log('Дождя нет!');
-    }
+    return '';
 }
 
-{
-    const mathPoints = Number(prompt('Введите кол-во баллов по математике'));
-    const rusPoints = Number(prompt('Введите кол-во баллов по русскому языку'));
-    const itPoints = Number(prompt('Введите кол-во баллов по информатике'));
-
-    const total = mathPoints + rusPoints + itPoints;
-
-    if (total >= 265) {
-        console.log('Поздравляю, вы поступили на бюджет!');
-    } else {
-        console.log('К сожалению, вы не поступили на бюджет!')
-    }
-}
-
-{
-    const value = prompt('Укажите сумму денег которые вы хотите снять');
-
-    if (value >= 100 && value % 100 === 0) {
-        console.log('Банкомат сможет выдать вам такую сумму!');
-    } else {
-        console.log('Банкомат не сможет выдать вам такую сумму!');
-    }
-}
+console.log(capitalize('привет Мир'));
