488a86cc17ae3d7f5d31d8119a3a6339363f2821
AidarS <nurotanec@bk.ru>
2022-03-26 17:54:09 +0600

    finished bonus tasks

---
+++ lesson04bonus/tasksHard_1.js
@@ -0,0 +1,15 @@
+'use strict';
+
+{
+    const income = Number(prompt('Введите свой общий доход'));
+
+    if (income >= 0 && income < 15000) {
+        console.log(`Налог будет ${income * 0.13} долларов`)
+    } else if (income >= 15000 && income < 50000) {
+        console.log(`Налог будет ${income * 0.2} долларов`)
+    } else if (income >= 50000) {
+        console.log(`Налог будет ${income * 0.3} долларов`)
+    } else {
+        console.log('Введены неправильные данные!')
+    }
+}
